ee81f4671832c843e6950094e25522ceb68aac27
tong12580 <yutong@rrjc.com>
2018-05-09 10:41:32 +0800

    更新版本

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { markdown } from 'markdown';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+console.log( markdown.toHTML( "Hello *World*!" ) );
